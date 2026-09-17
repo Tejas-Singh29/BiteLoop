3d8f14196da450a7b9b3573453f60a97d10562a2
Tejas Singh <mr.tejassingh3107@gmail.com>
2026-09-17 23:22:06 +0530

    initial commit

---
+++ backend/src/db/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const dns= require('dns');
+
+dns.setServers(["8.8.8.8", "8.8.4.4"])
+
+
+
+function connectDB() {
+    mongoose.connect(process.env.MONGODB_URI)
+        .then(() => {
+            console.log("MongoDB connected");
+        })
+        .catch((err) => {
+            console.log("MongoDB connection error:", err);
+        })
+}
+
+module.exports = connectDB;
